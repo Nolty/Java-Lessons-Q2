8261ca83765fb52d8fe12128541d3fcc05362e9d
nolty <nolty81@gmail.com>
2022-02-25 12:35:58 +0500

    Практическое задание к уроку №8

--- src/main/java/ru/geekbrains/HomeWorkApp.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains;
-
-public class HomeWorkApp {
-    public static void main(String[] args) {
-        System.out.println("Hello from Java");
-    }
-}
---
+++ src/main/java/ru/geekbrains/client/MainClient.java
@@ -0,0 +1,26 @@
+package ru.geekbrains.client;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainClient extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Чат");
+        stage.setScene(new Scene(root, 400, 400));
+        stage.show();
+        ChatController chatController = loader.getController();
+        stage.setOnCloseRequest(windowEvent -> {
+            chatController.close();
+            Platform.exit();
+            System.exit(0);
+        });
+    }
+}
